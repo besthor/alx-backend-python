e2509b782c8b48a9d4264ce2e07236988934c971
besthor <igbebestor7@gmail.com>
2023-07-07 09:37:06 +0000

    Basic annotations - floor

--- 0x00-python_variable_annotations/2-floor.py
+++ 0x00-python_variable_annotations/2-floor.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
-"""Basic annotations - floor"""
+''' Description:takes a float n as argument and returns the floor of the float
+    Parameter: n: float
+'''
 
 
 def floor(n: float) -> int:
-    """takes an argument and returns the floor of the float"""
-    return int(n)
+    ''' Return largest int value less than or equal to n. '''
+    return int(n) if n >= 0 else int(n) - 1
